33c42e6adc7019abcceba1a4abe27164f7eee69e
erianvc <erianvc.dev@gmail.com>
2020-02-04 21:26:44 -0500

    Base template

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let name;
+</script>
+
+<main class="flex flex-col max-w-lg p-4 my-0 mx-auto">
+	<h1 class="text-6xl text-red-500 font-hairline uppercase">Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+</style>
